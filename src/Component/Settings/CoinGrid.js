49f9381d6887f0419fead8fb9fea789078849d72
robertyarborough <admin@robert-yarborough.com>
2020-10-20 01:07:52 -0400

    Added responsive grid columns for cards

--- src/Component/Settings/CoinGrid.js
+++ src/Component/Settings/CoinGrid.js
@@ -6,8 +6,9 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 14px;
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  grid-gap: 15px;
+  margin-top: 40px;
 `;
 
 
